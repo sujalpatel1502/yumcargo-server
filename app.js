11ef622e865dffc3d82eb207e242b5f405e37c98
sujalpatel1502 <Sujalpatel@51>
2024-10-31 22:28:35 +0530

    solved port

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
+// import "dotenv/config"
+// import fastify from "fastify";
+// import { connectDB } from "./src/config/connect.js";
+// import { PORT } from "./src/config/config.js";
+// import { admin, buildAdminRouter } from "./src/config/setup.js";
+// import { registerRoutes } from "./src/routes/index.js";
+// import fastifySocketIO from 'fastify-socket.io'
+
+// // manav8424
+// // 9OjJQZw6VGem6v8u
+
+
+
+// // mongodb+srv://manav8424:1i93yY4iOge1lF2N@cluster0.ulpgk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// const start=async()=>{
+//     // console.log("envvv",process.env.MONGO_URI)
+
+//     await connectDB(process.env.MONGO_URI)
+//     const app=fastify();
+
+//     app.register(fastifySocketIO,{
+//         cors:{
+//             origin:"*"
+//         },
+//         pingInterval:10000,
+//         pingTimeout:5000,
+//         transports:["websocket"],
+//     })
+
+//     await registerRoutes(app)
+
+//     await buildAdminRouter(app)
+//     // const PORT=process.env.PORT || 3000;
+//     app.listen({port:PORT},(err,addr)=>{
+//         if(err){
+//             console.log("errr",err)
+//         }else{
+//             console.log(`yumcargo started on http://localhost:${PORT}${admin.options.rootPath}`)
+//         }
+//     });
+
+//     app.ready().then(()=>{
+//         app.io.on("connection",(socket)=>{
+//             console.log("A user connected ✅")
+
+//             socket.on("joinRoom",(orderId)=>{
+//                 socket.join(orderId);
+//                 console.log(`User Joined room ${orderId} 🏁`)
+//             })
+
+//             socket.on('disconnect',()=>{
+//                 console.log("User Disconnected ❌")
+//             })
+//         })
+//     })
+// }
+
+// start();
+
+
+
+
 import "dotenv/config"
 import fastify from "fastify";
 import { connectDB } from "./src/config/connect.js";
@@ -6,49 +68,48 @@ import { admin, buildAdminRouter } from "./src/config/setup.js";
 import { registerRoutes } from "./src/routes/index.js";
 import fastifySocketIO from 'fastify-socket.io'
 
-// manav8424
-// 9OjJQZw6VGem6v8u
-
-
-
-// mongodb+srv://manav8424:1i93yY4iOge1lF2N@cluster0.ulpgk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-const start=async()=>{
-    // console.log("envvv",process.env.MONGO_URI)
-
+const start = async () => {
     await connectDB(process.env.MONGO_URI)
-    const app=fastify();
+    const app = fastify();
 
-    app.register(fastifySocketIO,{
-        cors:{
-            origin:"*"
+    app.register(fastifySocketIO, {
+        cors: {
+            origin: "*"
         },
-        pingInterval:10000,
-        pingTimeout:5000,
-        transports:["websocket"],
+        pingInterval: 10000,
+        pingTimeout: 5000,
+        transports: ["websocket"],
     })
 
     await registerRoutes(app)
-
     await buildAdminRouter(app)
-    // const PORT=process.env.PORT || 3000;
-    app.listen({port:PORT},(err,addr)=>{
-        if(err){
-            console.log("errr",err)
-        }else{
-            console.log(`yumcargo started on http://localhost:${PORT}${admin.options.rootPath}`)
+
+    // Key changes here:
+    const port = process.env.PORT || PORT;
+    const host = "0.0.0.0"; // Important for Render
+
+    app.listen({
+        port: port,
+        host: host  // Bind to all network interfaces
+    }, (err, addr) => {
+        if (err) {
+            console.log("errr", err)
+            process.exit(1)
+        } else {
+            console.log(`yumcargo started on http://${host}:${port}${admin.options.rootPath}`)
         }
     });
 
-    app.ready().then(()=>{
-        app.io.on("connection",(socket)=>{
+    app.ready().then(() => {
+        app.io.on("connection", (socket) => {
             console.log("A user connected ✅")
 
-            socket.on("joinRoom",(orderId)=>{
+            socket.on("joinRoom", (orderId) => {
                 socket.join(orderId);
                 console.log(`User Joined room ${orderId} 🏁`)
             })
 
-            socket.on('disconnect',()=>{
+            socket.on('disconnect', () => {
                 console.log("User Disconnected ❌")
             })
         })
